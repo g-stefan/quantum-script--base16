b509e5469af9eaec910fa7b5fa2220720786ee4e
Grigore Stefan <g_stefan@yahoo.com>
2024-03-28 12:10:22 +0200

    Update

--- source/XYO/QuantumScript.Extension/Base16/Library.cpp
+++ source/XYO/QuantumScript.Extension/Base16/Library.cpp
@@ -64,7 +64,7 @@ namespace XYO::QuantumScript::Extension::Base16 {
 
 };
 
-#ifdef XYO_COMPILE_DYNAMIC_LIBRARY
+#ifdef XYO_PLATFORM_COMPILE_DYNAMIC_LIBRARY
 extern "C" XYO_QUANTUMSCRIPT_EXTENSION_BASE16_EXPORT void quantumScriptExtension(XYO::QuantumScript::Executive *executive, void *extensionId) {
 	XYO::QuantumScript::Extension::Base16::initExecutive(executive, extensionId);
 };
